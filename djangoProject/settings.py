aedaeec58cc57a030b6d4cfa65ea713b4c582c1c
“Anastasiya <anastasiya.kandratsiuk@gmail.com>
2024-05-03 22:37:53 +0200

    images replace

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -118,14 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATICFILES_DIRS = [BASE_DIR / "media/../static"]
-# Media files (uploaded user files)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
