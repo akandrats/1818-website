d391372a82d6944f6b3b4d38b92b96341396b8f8
“Anastasiya <anastasiya.kandratsiuk@gmail.com>
2024-05-03 20:32:54 +0200

    allowed host fix

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure--99x9@l0n4*sfv*ap=)k!9&oa1#c)z*-*1lcg_tfmwo)e!q9il
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
